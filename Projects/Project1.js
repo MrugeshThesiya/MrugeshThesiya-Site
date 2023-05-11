a2cc5a1c09982134c90f3d283ce084224873ab9a
Mrugesh Thesiya <132883293+MrugeshThesiya@users.noreply.github.com>
2023-05-11 18:34:45 +0530

    input output processing done

--- Projects/Project1.js
+++ Projects/Project1.js
@@ -1,9 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 function clickhandler(){
-    console.log("clicked!");
-    console.log("input", txtInput.value);
+   outputDiv.innerText = "afsgfras  " + txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickhandler)
