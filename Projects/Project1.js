6f6db9c251b04f6687bc56863ac96eae57308cdb
Mrugesh Thesiya <132883293+MrugeshThesiya@users.noreply.github.com>
2023-05-11 19:47:42 +0530

    final addition and done

--- Projects/Project1.js
+++ Projects/Project1.js
@@ -1,9 +1,24 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverUrl =  "	https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text)
+{
+    return serverUrl + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error occured " + error);
+    alert("There is something wrong with the server ! Please Try again later.");
+}
 
 function clickhandler(){
-   outputDiv.innerText = "afsgfras  " + txtInput.value;
+    var inputText = txtInput.value;
+   fetch(getTranslationUrl(inputText))
+   .then (response => response.json())
+   .then (json => {outputDiv.innerText = json.contents.translated; })
+   .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickhandler)
